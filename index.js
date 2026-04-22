1392012805f38a57e9e27666c86f03a7f7698f06
Marcial Becerra <marcialbr1820@gmail.com>
2026-04-21 18:13:47 -0600

    CAmbios

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = createServer(app);
 const io = new Server(server, {
   connectionStateRecovery: {},
   cors: {
-    origin: "https://chatify-ten-xi.vercel.app",
+    origin: "*",
     methods: ['GET', 'POST'],
     credentials: true
   }
@@ -30,9 +30,9 @@ const initDB = async () => {
           content TEXT
       );
     `);
-    console.log('Base de datos conectada y tabla lista');
+    console.log('DB Online');
   } catch (err) {
-    console.error('Error al conectar con la base de datos:', err);
+    console.error('DB Error:', err);
   }
 };
 initDB();
@@ -46,33 +46,28 @@ io.on('connection', async (socket) => {
 
   try {
     const offset = socket.handshake.auth.serverOffset || 0;
-
     const result = await pool.query(
       'SELECT id, content FROM messages WHERE id > $1 ORDER BY id ASC',
       [offset]
     );
-
     result.rows.forEach(row => {
       socket.emit('chat message', row.content, row.id);
     });
   } catch (e) {
-    console.error('Error al recuperar historial:', e);
+    console.error('History Error:', e);
   }
 
   socket.on('chat message', async (msg) => {
     if (!msg) return;
-
     try {
       const result = await pool.query(
         'INSERT INTO messages (content) VALUES ($1) RETURNING id',
         [msg]
       );
-
       const lastId = result.rows[0].id;
-
       io.emit('chat message', msg, lastId);
     } catch (e) {
-      console.error('Error al insertar mensaje:', e);
+      console.error('Insert Error:', e);
     }
   });
 
@@ -83,5 +78,5 @@ io.on('connection', async (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log(`Server on port ${PORT}`);
 });
